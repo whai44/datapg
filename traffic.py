9316540f59374bd0d6f11eea92610cb7c4ceeff9
whai44 <108253673+whai44@users.noreply.github.com>
2023-03-30 01:27:06 +0700

    Add files via upload

--- traffic.py
+++ traffic.py
@@ -9,6 +9,7 @@ import dash_core_components as dcc
 from dash.dependencies import Output, Input
 import pandas as pd
 from app import app
+import plotly.graph_objs as go
 
 
 #########################Traffic signal sensor at kasemrad junction###############################
@@ -27,18 +28,23 @@ for i in range (len(jsonks)):
             'phase_8':jsonks[i]['data']['phase_8']})
 df_ks = pd.DataFrame(data_ks)
 df_ks.timestamp = pd.to_datetime(df_ks.timestamp)
-df_ks['day_name']=df_ks['timestamp'].dt.day_name()
+df_ks['day']=df_ks['timestamp'].dt.day_name()
 df_ks.set_index('timestamp',inplace = True)
-df_ksm = df_ks.between_time('7:00','10:00').groupby(by=['day_name']).mean()
-df_kse = df_ks.between_time('16:00','19:00').groupby(by=['day_name']).mean()
+df_ksm = df_ks.between_time('7:00','10:00').groupby(by=['day']).mean()
+df_kse = df_ks.between_time('16:00','19:00').groupby(by=['day']).mean()
 df_ksm = df_ksm.reset_index()
 df_kse = df_kse.reset_index()
-dfm = df_ksm.set_index('day_name')
-dfe = df_kse.set_index('day_name')
+
 
 #######################################Layout##############################################
 tf_layout = html.Div(
     [
+    dbc.Row([
+        html.Div([
+            # dcc.Input(),
+            html.Br(),
+             ])
+    ]),
 
     dbc.Row(
         dbc.Col(html.H3("Traffic Signal Phase",
@@ -46,32 +52,26 @@ tf_layout = html.Div(
                 width=12)
     ),
 
-
-
     dbc.Row([
-        html.Img(src='https://i.ibb.co/d6vvqbg/2565-11-03-01-22-06.png', alt='image',height="300",width="9")
+        html.Img(src='https://i.ibb.co/d6vvqbg/2565-11-03-01-22-06.png', alt='image',height="240",width="6")
     ]),
     dbc.Row([
         html.Div([
             # dcc.Input(),
             html.Br(),
             html.Br(),
-            html.Br(),
-            html.Br(),
-            ])
+        ])
     ]),
     dbc.Row([
 
-       dcc.Dropdown(id='my-dpdn4', multi=False, value=['MON'],
-                         options=[{'label':x, 'value':x}
-                                  for x in df_ksm['day_name'].to_list()],
-                         )
-]),
+       dcc.Dropdown(
+        id='day-dropdown',
+        options=[{'label': day, 'value': day} for day in df_ksm['day'].unique()],
+        value=df_ksm['day'].unique()[0]
+    )]),
     dbc.Row([
         html.Div([
             # dcc.Input(),
-            html.Br(),
-            html.Br(),
             html.Br(),
             html.Br(),
             ])
@@ -81,25 +81,22 @@ tf_layout = html.Div(
         dbc.Col(html.H4("mornig rush hour",
                         className='text-center text-warning mb-4'),
                 width=6),
-
-
         dbc.Col(html.H4("evening rush hour",
                         className='text-center text-warning mb-4'),
                 width=6),
-
-        ]),
+                ]),
 
 
     dbc.Row([
         dbc.Col([
             
-            dcc.Graph(id='line-fig3')
+            dcc.Graph(id='bar-chart-1')
         ],width=6#width={'size':5, 'offset':0, 'order':2},
            
         ),
         dbc.Col([
            
-            dcc.Graph(id='line-fig4', figure={})
+            dcc.Graph(id='bar-chart-2', figure={})
         ], #width={'size':5, 'offset':0, 'order':2},
            width=6),
     
@@ -109,15 +106,21 @@ tf_layout = html.Div(
 ##########################callback##########################################
 
 @app.callback(
-    Output(component_id='line-fig3', component_property='figure'),
-    [Input(component_id='my_dpdn4', component_property='value')]
-)
+    dash.dependencies.Output('bar-chart-1', 'figure'),
+    [dash.dependencies.Input('day-dropdown', 'value')])
 
-def update_graph(my_dpdn4):
-    dfmm = dfm.transpose()
-    dfmm = dfmm.reset_index()
+def update_bar_chart(selected_day):
+    filtered_data = df_ksm[df_ksm['day'] == selected_day]
+    values = filtered_data.iloc[0, 1:].tolist()
+    bar_chart = go.Bar(x=['Phase 1', 'Phase 2', 'Phase 3', 'Phase 4'], y=values)
+    return {'data': [bar_chart]}
     
-
-    piechart=px.pie(dfmm , names = 'index' , values = 'my_dpdn4')
-
-    return (piechart)
+    
+@app.callback(
+    dash.dependencies.Output('bar-chart-2', 'figure'),
+    [dash.dependencies.Input('day-dropdown', 'value')])
+def update_bar_chart_2(selected_day):
+    filtered_data = df_kse[df_kse['day'] == selected_day]
+    values = filtered_data.iloc[0, 1:].tolist()
+    bar_chart = go.Bar(x=['Phase 1', 'Phase 2', 'Phase 3', 'Phase 4'], y=values)
+    return {'data': [bar_chart]}
